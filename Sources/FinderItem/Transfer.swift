ac062e1f5f312d9622e18653eed9114bd92d29c5
Vaida <developer@vaidaf.net>
2024-08-05 21:18:27 +1000

    integrate to new Metalmanager

--- Sources/FinderItem/Transfer.swift
+++ Sources/FinderItem/Transfer.swift
@@ -15,8 +15,16 @@ extension FinderItem: Transferable {
     ///
     /// On dragged, the *original* file is used. Which means that one file can only be dragged once.
     public static var transferRepresentation: some TransferRepresentation {
-        ProxyRepresentation(exporting: \.url) { url in
-            FinderItem(at: url)
+        ProxyRepresentation(exporting: \.url, importing: FinderItem.init)
+        FileRepresentation(importedContentType: .image) { received in
+            guard !received.isOriginalFile else { return FinderItem(at: received.file) }
+            let copy = FinderItem.temporaryDirectory.appending(path: received.file.lastPathComponent)
+            try copy.removeIfExists()
+            
+            // must use file manager, otherwise bad access
+            try FileManager.default.copyItem(at: received.file, to: copy.url)
+            
+            return copy
         }
     }
     
