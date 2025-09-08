561d96732b3ef2fa75af897393061f1a6cd2eaf3
Vaida <developer@vaidaf.net>
2025-09-08 17:33:26 +1000

    updates dependencies

--- Client/main.swift
+++ Client/main.swift
@@ -10,4 +10,12 @@ import FinderItem
 import Foundation
 import DetailedDescription
 
+let root = FinderItem(at: "~")
+
+for child in try root.children(range: .enumeration) {
+    guard let tags = try? child.load(.tags) else { continue  }
+    if tags.contains(where: { $0.hasPrefix("黄色") }) {
+        print(child)
+    }
+}
 #endif
