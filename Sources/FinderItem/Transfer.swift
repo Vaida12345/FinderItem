bbc4259df00aeba465f5b1a26ebe19b2b744676c
Vaida <developer@vaidaf.net>
2026-05-14 03:14:25 +1000

    updates docs

--- Sources/FinderItem/Transfer.swift
+++ Sources/FinderItem/Transfer.swift
@@ -13,13 +13,14 @@ extension FinderItem: Transferable {
     
     /// The transferable representations.
     ///
-    /// When the dragged item is a file or a directory url, the original item is used. When the item is data, a temporary copy is created, and you should remove the copy once finished.
+    /// When the dragged item is a file or a directory url, the original item is used. When the item is data, a temporary copy may be created.
     public static var transferRepresentation: some TransferRepresentation {
         ProxyRepresentation(exporting: \.url, importing: { FinderItem(_url: $0) })
         FileRepresentation(importedContentType: .data, shouldAttemptToOpenInPlace: true) { received in // import type cannot be `item`, otherwise it would attempt to copy any file (folder) dragged into `dragDestination`.
-            guard !received.isOriginalFile else {
+            if received.isOriginalFile {
                 return FinderItem(_url: received.file)
             }
+            
             let copy = FinderItem.temporaryDirectory.appending(path: received.file.lastPathComponent).generateUniquePath()
             
             // must use file manager, otherwise bad access
